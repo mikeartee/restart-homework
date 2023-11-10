301b80e028cc1d4b43d81ca0c1d731245209306d
mikeartee <75613550+mikeartee@users.noreply.github.com>
2023-11-10 16:35:18 +1300

    Update CarMike.py
    Description added.

--- CarMike.py
+++ CarMike.py
@@ -1,3 +1,5 @@
+#Define Car as Parent Class
+
 class Car:
     def __init__(self, brand, milage_km):                                   
         self.brand=brand
@@ -6,7 +8,7 @@ class Car:
     def drive(self, distance_km):
         self.milage_km = self.milage_km + distance_km
 
-
+#Define Child Classes ElectricCar and IceCar - give them Drive variables then calculate fuel level and distance to get range and fuel level after travelling distance. 
 class ElectricCar(Car):
     def __init__(self, brand, milage_km, range, fuel_level):
         super().__init__(brand, milage_km)
@@ -16,7 +18,7 @@ class ElectricCar(Car):
         super().drive(distance_km)
         self.range= self.range-distance_km
         self.fuel_level = (distance_km*self.fuel_level/100)
-
+#Similar with ICE but fuel consumption and fuel level.
 class IceCar(Car):
     def __init__(self, brand, milage_km, fuel_consumption, fuel_level):
         Car.__init__(self, brand, milage_km)
@@ -26,13 +28,15 @@ class IceCar(Car):
         super().drive(distance_km)
         self.fuel_level= self.fuel_level-distance_km
         self.fuel_level = (distance_km*self.fuel_consumption/100)
-
+#Test Electric Car Drive
 my_ev = ElectricCar("Tesla", 80000, 300, 30)
 my_ev.drive(distance_km=32.5)
 my_ev1 = ElectricCar("Tesla", 80000, 300, 30)
 my_ev1.drive(distance_km=75)
 my_ev2 = ElectricCar("Tesla", 80000, 300, 30)
 my_ev2.drive(distance_km=150)
+
+#Test IceCar Drive
 
 my_icecar = IceCar("Mitsubishi", 25000, 7, 30)
 my_icecar.drive(distance_km=150)
@@ -41,6 +45,7 @@ my_icecar1.drive(distance_km=75)
 my_icecar2 = IceCar("Mitsubishi", 25000, 7, 30)
 my_icecar2.drive(distance_km=32.5)
 
+#Print wasn't sure if it was needed to number each time, but did so anyways.
 print(my_ev.__dict__)
 print("")
 print(my_ev1.__dict__)
